6baeb0113a98169d5a99a4ff91d9b0d50c59f3e3
vcaesar <vzvway@gmail.com>
2024-08-16 11:22:49 -0700

    Update: update examples and CI

--- examples/main.go
+++ examples/main.go
@@ -8,14 +8,22 @@ import (
 )
 
 func main() {
+	// save images
 	img := robotgo.CaptureImg()
 	img1 := robotgo.CaptureImg(18, 4, 20, 20)
+
 	rs := gcv.FindAllImg(img1, img)
-	fmt.Println("find: ", rs[0].TopLeft.Y, rs[0].Rects.TopLeft.X, rs[0].ImgSize.H)
+	if len(rs) > 0 {
+		fmt.Println("find: ", rs[0].TopLeft.Y, rs[0].Rects.TopLeft.X, rs[0].ImgSize.H)
+	}
 	fmt.Println("find: ", rs)
 
 	m1, _ := gcv.ImgToMat(img)
 	m2, _ := gcv.ImgToMat(img1)
+
 	rs = gcv.FindAllTemplate(m1, m2, 0.8)
 	fmt.Println("find: ", rs)
+
+	rs = gcv.FindAllSift(m1, m2)
+	fmt.Println("find sift: ", rs)
 }
